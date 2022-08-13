d59591909b0fc0960778aab51381415ae98edc21
dou <cyk.dou@gmail.com>
2022-08-13 16:51:38 +0800

    fmt

--- src/leetcode/editor/cn/medium/list/mod.rs
+++ src/leetcode/editor/cn/medium/list/mod.rs
@@ -1,2 +1,2 @@
-mod odd_even_linked_list;
 pub mod add_two_numbers;
+mod odd_even_linked_list;
--- src/test_std/test_thread/mod.rs
+++ src/test_std/test_thread/mod.rs
@@ -1,3 +1,3 @@
-mod test_thread;
-mod test_channel;
 mod proof_of_work_demo;
+mod test_channel;
+mod test_thread;
